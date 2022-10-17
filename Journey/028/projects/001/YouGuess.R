9ee2fe14ffb4fdf58d87101fc7b32078019ed13f
akimwong <91024564+akimwong@users.noreply.github.com>
2022-10-17 11:39:20 +0200

    Update YouGuess.R

--- Journey/028/projects/001/YouGuess.R
+++ Journey/028/projects/001/YouGuess.R
@@ -2,7 +2,7 @@ youGuess <- function() {
   print('Give me a range of numbers...')
   rangeInf <- as.integer(readline(prompt='Inferior number: '))
   rangeSup <- as.integer(readline(prompt='Superior number: '))
-  compuNum <- sample(rangeInf:rangeSup,1)  # '1' is the number of samples
+  compuNum <- sample(rangeInf:rangeSup, 1)  # '1' is the number of samples
   # print(compuNum)
   compareNum <- function(compuNum) {
     yourNum <- as.integer(readline(prompt='Give me a number: '))
@@ -13,7 +13,7 @@ youGuess <- function() {
     } else if (yourNum == compuNum) {
       print ('Perfect! Computer number is the same as your number')
       print ('End of Game')
-      playAgain <- (readline(prompt='Play again? (y/n): '))
+      playAgain <- readline(prompt='Play again? (y/n): ')
       if (playAgain == 'y'){
         youGuess()
       } else {
@@ -24,16 +24,17 @@ youGuess <- function() {
         print ('Ups! Computer number is lower than your number')
     }
   }
-  #my_range <- 1:10
   for (i in 1:10) {
     loop = TRUE
     loop = compareNum(compuNum)
     if (loop == FALSE) {
+      print('Thank you, Bye!')
       break
     }
   }
-  print('Sorry, no more opportunities')
+  if (loop != FALSE) {
+    print('Sorry, no more opportunities')
+  }
 }
-
 
 youGuess()
