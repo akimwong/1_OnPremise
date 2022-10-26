c13f262bbe4a069cd4a37cdedb2304d73d633a82
akimwong <91024564+akimwong@users.noreply.github.com>
2022-10-26 08:36:40 +0200

    Update picnic.py

--- Journey/028/projects/004/picnic.py
+++ Journey/028/projects/004/picnic.py
@@ -61,6 +61,32 @@ def main():
 
     print(f"You are bringing {food}")
 
+'''
+<<<<< SOLUTION OF THE WRITER >>>>>
+
+def main():
+"""Make a jazz noise here"""
+
+args = get_args()
+items = args.item
+num = len(items)
+
+if args.sorted:
+    items.sort()
+
+bringing = ''
+if num == 1:
+    bringing = items[0]
+elif num == 2:
+    bringing = ' and '.join(items)
+else:
+    items[-1] = 'and ' + items[-1]
+    bringing = ', '.join(items)
+
+print('You are bringing {}.'.format(bringing))
+'''
+    
+    
 # --------------------------------------------------
 if __name__ == '__main__':
     main()
